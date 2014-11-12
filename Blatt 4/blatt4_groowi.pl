6a99a281e3f2e181236ab4d50076805ddbbc2d0b
Suchti <frederikwille@arcor.de>
2014-11-12 16:56:18 +0100

    Blatt4 Aufg1 angefangen

---
+++ Blatt 4/blatt4_groowi.pl
@@ -0,0 +1,22 @@
+%%%%% Aufgabe 1
+
+%% A ist das Geburtsdatum von B
+%
+
+%% A ist im Turnier gegen B angetrete
+% symmetrisch
+
+%% A is älter als B
+% 
+
+%% A und B sind Geschwister
+% symmetrisch
+
+%% A ist eine (echte oder unechte) Teilmenge von B
+% kann reflexiv und symmetrisch sein, wenn A=B
+
+%% A und B sind in der gleichen Straße
+% immer symmetrisch, kann auch reflexiv sein, wenn A=B
+
+%%%%% Aufgabe 2
+
