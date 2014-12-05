62b9dd511334177dd88c9247613ac1e701b67589
Sascha Graeff <sascha@blackicegames.de>
2014-12-05 14:45:16 +0100

    Soweit, so gut.

--- Blatt 7/blatt07_groowi.pl
+++ Blatt 7/blatt07_groowi.pl
@@ -1,3 +1,24 @@
 %%%%%% Sascha Graeff, Frederik Wille
 
 %%%%% Aufgabe 1
+% ?- [A,m,A] = [B,B,n]
+% 3-elementige Liste = 3-elementige Liste
+% A = B
+% m = B => A = B = m
+% A = n => A = m =\= n
+% An A wird erst m gebunden, dann soll n gebunden werden.
+% Aufgrund dieser Kollision schlägt die Unifikation fehl.
+
+% ?- [0, [T, 1, T], S, [1, S, 1]] =
+     [M, N, [M, 1, N], [1, 0, 1], N]
+% 4-elementige Liste =\= 5-elementige Liste
+% Die Unifikation schlägt fehl, da die Listen unterschiedlich lang sind.
+
+% ?- [D,D|E] = [[P,b], [a|Q], [Q,P], [P,Q], [P|Q], [Q,P]]
+% D = [P,b]
+% D = [a|Q]
+% [P,b] = [a|Q]
+% P = a
+% [b] = Q
+% E = [[Q,P], [P,Q], [P|Q], [Q,P]]
+% 
