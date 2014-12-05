b2c191f64a36717fe7595b7b0b0ea791493cd93d
Sascha Graeff <sascha@blackicegames.de>
2014-12-05 16:11:32 +0100

    2.2 fast fertig.

--- Blatt 7/blatt07_groowi.pl
+++ Blatt 7/blatt07_groowi.pl
@@ -38,12 +38,13 @@
 my_numlist(Low, Low, [Low]).
 my_numlist(Low, High, List) :-
     once((
-    Low < High,
-    LessLow is Low + 1,
-    my_numlist(LessLow, High, Tail),
-    List = [Low|Tail])).
+        Low < High,
+        LessLow is Low + 1,
+        my_numlist(LessLow, High, Tail),
+        List = [Low|Tail]
+    )).
 
-% Tests (numlist hatte immer jeweils das Gleiche Ergebnis):
+% Tests (numlist hatte immer jeweils das gleiche Ergebnis):
 % ?- my_numlist(1,1,Liste).
 %    Liste = [1].
 % ?- my_numlist(1,5,Liste).
@@ -58,3 +59,47 @@ my_numlist(Low, High, List) :-
 %    ERROR: Type error: `integer' expected, found `a'
 
 %%%% 2)
+% nth0(+Index, ?List, ?Elem)
+% Unsere Variante ist mit +Index definiert.
+% Das originale nth0 hat ?Index, wirft aber einen Fehler,
+% wenn Index ungebunden ist. Das fanden wir weniger klug als +Index.
+
+my_nth0(0, List, Elem) :-
+    List = [Elem|_].
+my_nth0(Index, List, Elem) :-
+    once((
+        Index > 0,
+        List = [_|Tail],
+        IndexPlusOne is Index - 1,
+        my_nth0(IndexPlusOne, Tail, Elem)
+    )).
+
+% Test (nth0 hatte immer jeweils das gleiche Ergebnis):
+% ?- my_nth0(0, [a,b,c,d,e], a).
+%    true;
+%    false.
+% ?- my_nth0(1, [a,b,c,d,e], a).
+%    false.
+% ?- my_nth0(0, [a,b,c,d,e], b).
+%    false.
+% ?- my_nth0(2, [a,b,c,d,e], Elem).
+%    Elem = c.
+% ?- my_nth0(3, Liste, a).
+%    Liste = [_G19528, _G19531, _G19534, a|_G19538].
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
