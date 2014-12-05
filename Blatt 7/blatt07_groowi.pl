fd9e419503146cae231aadf00a002b5f2a12b8b6
Sascha Graeff <sascha@blackicegames.de>
2014-12-05 15:23:44 +0100

    1.1 yeah.

--- Blatt 7/blatt07_groowi.pl
+++ Blatt 7/blatt07_groowi.pl
@@ -10,7 +10,7 @@
 % Aufgrund dieser Kollision schlägt die Unifikation fehl.
 
 % ?- [0, [T, 1, T], S, [1, S, 1]] =
-%     [M, N, [M, 1, N], [1, 0, 1], N]
+%    [M, N, [M, 1, N], [1, 0, 1], N]
 % 4-elementige Liste =\= 5-elementige Liste
 % Die Unifikation schlägt fehl, da die Listen unterschiedlich lang sind.
 
@@ -33,3 +33,28 @@
 % W = [r, r].
 
 %%%%% Aufgabe 2
+%%%% 1)
+% my_numlist(+Low, +High, -List)
+my_numlist(Low, Low, [Low]).
+my_numlist(Low, High, List) :-
+    once((
+    Low < High,
+    LessLow is Low + 1,
+    my_numlist(LessLow, High, Tail),
+    List = [Low|Tail])).
+
+% Tests (numlist hatte immer jeweils das Gleiche Ergebnis):
+% ?- my_numlist(1,1,Liste).
+%    Liste = [1].
+% ?- my_numlist(1,5,Liste).
+%    Liste = [1,2,3,4,5].
+% ?- my_numlist(1,0).
+%    false.
+
+% my_numlist unterscheidet sich von numlist lediglich in der Fehlerbehandlung:
+% ?- my_numlist(1,a,List).
+%    ERROR: </2: Arithmetic: `a/0' is not a function
+% ?- numlist(1,a,List).
+%    ERROR: Type error: `integer' expected, found `a'
+
+%%%% 2)
