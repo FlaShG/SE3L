3c2181164268b23a010faa6d27b70f007f67aada
Sascha Graeff <sascha@blackicegames.de>
2015-01-09 17:29:59 +0100

    10.1.1 Trainingsmenge.

--- Blatt 10/blatt10_groowi.pl
+++ Blatt 10/blatt10_groowi.pl
@@ -9,7 +9,7 @@
 % Testperson nennt, was vom Inhalt der Plakate hängen geblieben ist.
 
 % Klassen:
-% top = das Werbeplakat wurde zuerst angesehen
+% top = das Werbeplakat wurde sehr gründlich angesehen
 % ok = das Werbeplakat wurde wahrgenommen
 % flop = das Werbeplakat wurde kaum oder gar nicht wahrgenommen
 
@@ -25,6 +25,20 @@
 % (es war immer nur maximal eine Person auf dem Plakat)
 
 % Tier(e) auf dem Plakat (Anzahl)
+
+:- dynamic d/2.
+d(top, [60, 2, 2]).
+d(top, [55, 2, 0]).
+d(top, [57, -2, 4]).
+d(ok, [20, 2, 1]).
+d(ok, [52, -1, 1]).
+d(ok, [80, 0, 5]).
+d(ok, [55, -1, 0]).
+d(ok, [40, -2, 0]).
+d(flop, [5, 1, 0]).
+d(flop, [80, 0, 0]).
+d(flop, [67, -1, 0]).
+d(flop, [80, 0, 1]).
 
 
 %%%% 2)
