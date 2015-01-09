ecd0d797c2b1a7c21534ce316c5a183eb3d2ad02
Sascha Graeff <sascha@blackicegames.de>
2015-01-09 17:00:28 +0100

    10.1.1 gemacht

--- Blatt 10/blatt10_groowi.pl
+++ Blatt 10/blatt10_groowi.pl
@@ -3,3 +3,28 @@
 
 %%%%% Aufgabe 1
 %%%% 1)
+
+% Es geht um Werbeplakate, genauer um eine Studie zur Wirksamkeit dieser.
+% Die Plakate werden gleichzeitig für eine sehr kurze Zeit enthüllt, und die
+% Testperson nennt, was vom Inhalt der Plakate hängen geblieben ist.
+
+% Klassen:
+% top = das Werbeplakat wurde zuerst angesehen
+% ok = das Werbeplakat wurde wahrgenommen
+% flop = das Werbeplakat wurde kaum oder gar nicht wahrgenommen
+
+% Merkmale:
+% Helligkeit des Plakats (Werte von 0 (dunkel) bis 100 (hell))
+
+% Person auf dem Plakat:
+% -2 = leicht bekleideter Mann
+% -1 = Mann
+% 0 = keine Person
+% 1 = Frau
+% 2 = leicht bekleidete Frau
+% (es war immer nur maximal eine Person auf dem Plakat)
+
+% Tier(e) auf dem Plakat (Anzahl)
+
+
+%%%% 2)
